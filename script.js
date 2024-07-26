7343f92c7038ed5e5a1bc57e69e5993b73f49e56
SABINISPRO111 <102422626+SABINISPRO111@users.noreply.github.com>
2024-07-26 22:17:56 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
 // script.js
-
 function toggleMenu() {
     const sideMenu = document.getElementById('sideMenu');
-    if (sideMenu.style.width === '250px') {
-        sideMenu.style.width = '0';
-    } else {
-        sideMenu.style.width = '250px';
-    }
+    sideMenu.classList.toggle('open');
 }
