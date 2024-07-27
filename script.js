715c4cb3f0231b407ae42999e8849a0f44f77396
SABINISPRO111 <102422626+SABINISPRO111@users.noreply.github.com>
2024-07-27 07:50:35 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // script.js
 function toggleMenu() {
     const sideMenu = document.getElementById('sideMenu');
+    const overlay = document.getElementById('overlay');
     sideMenu.classList.toggle('open');
+    overlay.classList.toggle('open');
 }
